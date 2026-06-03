52962f1a179cd1dfd1c0a0b8e34c07afdb54502f
asheer_ahmad <asheerahmadkmt@gmail.com>
2026-06-03 16:46:48 +0530

    data cleaning notebook complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pulsemap-india',
+    version='0.1.0',
+    author='Asheer Ahmad',
+    packages=find_packages(),
+    install_requires=[]
+)
