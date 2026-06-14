c38f628755555377100543efc947041298a71e43
asheer_ahmad <asheerahmadkmt@gmail.com>
2026-06-15 02:13:32 +0530

    Remove setup.py file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='pulsemap-india',
-    version='0.1.0',
-    author='Asheer Ahmad',
-    packages=find_packages(),
-    install_requires=[]
-)
